fde8d55e27b5f432baed4d21aa60d3ff793cea9e
TiM3er <33333118+TiM3er@users.noreply.github.com>
2017-11-04 17:04:35 +0100

    complete

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,18 @@
 package main;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		String napis = scan.nextLine(); 
+		byte[] bity = napis.getBytes(StandardCharsets.US_ASCII);
+		for(int i =0;i<bity.length;i++) {
+			System.out.println("Znak: "+napis.charAt(i) +" Dziesietny: "+bity[i]+ " Hex: "+ Integer.toHexString(bity[i]));
+		}
 	}
 
 }
